2a0a17180776418fc92f63b2fbfd6c6844cbbbb7
Julia Spriggs <wildcode@julia-spriggs.fr>
2023-02-27 17:58:35 +0100

    added basic routing and imports

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Page
+import ArgonautsListPage from "./pages/ArgonautsListPage";
+import AddArgonaut from "./components/AddArgonaut";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/argonauts" element={<ArgonautsListPage />} />
+          <Route path="/argonauts/add-argonaut" element={<AddArgonaut />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
