5052ec5dfde8b22270b60fbf8f4b1175b9d8e801
Julia Spriggs <wildcode@julia-spriggs.fr>
2023-02-27 18:00:27 +0100

    added the add argonaut component

---
+++ src/components/AddArgonaut.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
+
+function AddArgonaut(props) {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { name: name };
+
+    // Get the token from localStorage
+    const storedToken = localStorage.getItem("authToken");
+
+    // Send token through the request "authorization" headers
+    axios
+      .post(`${API_URL}/api/argonauts`, requestBody, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        // Reset the state
+        setName("");
+        props.refreshArgonauts();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      <h2>Ajouter un(e) Argonaute</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>Nom de l'argonaute:</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <button type="submit">Envoyer</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddArgonaut;
